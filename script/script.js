6045101383627de96d709a9d524158045e7f1c99
Umberto Amenta <umberto.amenta@hotmail.it>
2025-01-09 16:47:04 +0100

    feat: +album

---
+++ script/script.js
@@ -0,0 +1,29 @@
+// DOM ELEMENTS
+const boardDom = document.getElementById("board")
+
+// FUNCTIONS
+
+
+// MAIN PROGRAM
+// page load
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+.then(function(res) {
+    console.log(res)
+    const album = res.data
+    console.log(album)
+    album.forEach(photo => {
+        console.log(photo)
+        boardDom.innerHTML += `
+            <div class="card">
+                <div class="pin">
+                    <img src="img/pin.svg" alt="">
+                </div>
+                <img src="${photo.url}" alt="">
+                <p>${photo.title}</p>
+            </div>
+        `
+    });
+})
+
+
+// EVENTS
