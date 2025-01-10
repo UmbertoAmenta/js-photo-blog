ca8fcd174c667fca91f316b836de2392a329a32c
Umberto Amenta <umberto.amenta@hotmail.it>
2025-01-10 19:48:22 +0100

    feat: overlay, closing without button; minor changes; cleaning code

--- script/script.js
+++ script/script.js
@@ -3,8 +3,6 @@ const boardDom = document.getElementById("board")
 const btnDom = document.getElementById("btn")
 const hoverDom = document.getElementById("overlay")
 
-// FUNCTIONS
-
 
 // MAIN PROGRAM
 // page load
@@ -14,32 +12,54 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     album.forEach(photo => {
         boardDom.innerHTML += `
             <div class="card">
-            <div class="pin">
-            <img src="img/pin.svg" alt="">
-            </div>
-            <img src="${photo.url}" alt="loading...">
-            <p>${photo.title}</p>
+                <div class="pin">
+                 <img src="img/pin.svg" alt="">
+             </div>
+             <img src="${photo.url}" alt="loading...">
+             <p>${photo.title}</p>
             </div>
         `
     })
-        
+
     const cardsDom = document.querySelectorAll(".card")
-    
+        
     cardsDom.forEach(cardDom => {
+        const pinDom = cardDom.querySelector(".pin")
+
+        // x aggiunta titolo
         console.log(cardDom)
+        const titleOverlayDom = cardDom.querySelector("p").innerHTML
+
         cardDom.addEventListener("click", function () {
-            hoverDom.classList.remove("hidden")
             const photo = (cardDom.querySelector("div + img")).src
-            console.log(photo)
+
+            hoverDom.classList.remove("hidden")
             hoverDom.querySelector("img").src = photo
-        })        
+            console.log(cardDom.querySelector("div + img"))
+            cardDom.classList.add("card-clicked")
+            pinDom.classList.add("hidden")
+
+            // x aggiunta titolo
+            console.log(cardDom.querySelector("p"))
+            console.log(titleOverlayDom)
+
+            // cardDom.querySelector("div + img").classList.add("shadow")
+        })
+
+        // close overlay on click
+        hoverDom.addEventListener("click", function () {
+            hoverDom.classList.add("hidden")
+            cardDom.classList.remove("card-clicked")
+            pinDom.classList.remove("hidden")
+            // cardDom.querySelector("div + img").classList.remove("shadow")
+
+        })
     })
-})
-            
+})    
 
 
 // EVENTS
-
+// btn click (richiesto)
 btnDom.addEventListener("click", function () {
     hoverDom.classList.add("hidden")
 })
